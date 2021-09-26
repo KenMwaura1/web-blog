caa844475b48e714afc82d764ae1bb19767c8d69
Kennedy Ngugi Mwaura <kemwaura@gmail.com>
2021-09-26 13:52:41 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 #Includes tests for the blog project
 #TODO: Write tests for the web blog project
 import pytest
+def capital_case(x):
+    return x.capitalize()
 
 def test_capital_case():
     assert capital_case('blog') == 'Blog'
